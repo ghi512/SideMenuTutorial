9ffa28a9e3c39b1be85a5e16d9bde2dd15bddb24
김민지 <ghi512151@gmail.com>
2024-11-19 16:03:57 +0100

    Initial Commit

---
+++ SideMenuTutorial/SideMenuTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuTutorialApp.swift
+//  SideMenuTutorial
+//
+//  Created by 김민지 on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SideMenuTutorialTests/SideMenuTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuTutorialTests.swift
+//  SideMenuTutorialTests
+//
+//  Created by 김민지 on 11/19/24.
+//
+
+import Testing
+@testable import SideMenuTutorial
+
+struct SideMenuTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
